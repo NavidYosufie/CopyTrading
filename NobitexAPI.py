a45f7e29978eed7cdf173eaf5c731c6ec04505a3
navid <navid.yosufie4@gmail.com>
2024-06-24 18:34:04 +0330

    add get detail order

--- NobitexAPI.py
+++ NobitexAPI.py
@@ -6,20 +6,31 @@ class NobitexAPI:
         self.base_url = base_url
         self.token = token
 
-    def get_wallet_balance(self):
+    def get_wallet_balance(self, data):
         url = f'{self.base_url}users/wallets/balance'
-        payload = {'currency': 'ltc'}
-        response = requests.request('POST', url, headers=self.token, data=payload)
+        response = requests.post(url, headers=self.token, data=data)
         if response.status_code == 200:
             return response.json()
-        return response.status_code
+        return response.raise_for_status()
 
-    def get_user_order(self):
+    def get_list_order(self):
         url = f'{self.base_url}market/orders/list'
-        response = requests.request('GET', url, headers=self.token)
+        response = requests.get(url, headers=self.token)
+        return response.json()
+
+    def get_detail_order(self, status, type, srcCurrency, dstCurrency, details):
+        url = f'{self.base_url}market/orders/list?'
+        payload = {
+            'status': status,
+            'type': type,
+            'srcCurrency': srcCurrency,
+            'dstCurrency': dstCurrency,
+            'details': details
+        }
+        response = requests.get(url, headers=self.token, data=payload)
         if response.status_code == 200:
             return response.json()
-        return response.status_code
+        return response.raise_for_status()
 
     def set_order(self, type, execution, srcCurrency, dstCurrency, amount, price, stopPrice, stopLimitPrice):
         url = f'{self.base_url}market/orders/add'
@@ -31,18 +42,20 @@ class NobitexAPI:
             'amount': amount,
             'price': price,
             'stopPrice': stopPrice,
-            'stopLimitPrice': stopLimitPrice
+            'stopLimitPrice': stopLimitPrice,
         }
         response = requests.request('POST', url, headers=self.token, data=payload)
-        status = response.json()
-        return status
+        return response.json()
+
 
 
 api = NobitexAPI('https://api.nobitex.ir/', {'Authorization': 'Token ******'})
 
-balance = api.get_wallet_balance()
+balance = api.get_wallet_balance({'currency': 'rls'})
 print(balance)
-orders = api.get_user_order()
-print(orders)
-new_order = api.set_order()
-print(new_order)
+listOrder = api.get_list_order()
+print(listOrder)
+detailOrder = api.get_detail_order('all', 'sell', 'btc', 'rls', 1)
+print(listOrder)
+set_order = api.set_order('buy', 'limit', 'btc', 'usdt', 1, 64000, 67000, 63000)
+print(set_order)
